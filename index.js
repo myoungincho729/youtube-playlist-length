f32476632e11b6a65ac44d637aef4aa22e116057
myoungin <myoungin729@gmail.com>
2022-08-23 14:09:57 +0900

    2nd heroku

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const { get_list_id, play }  = require('./routes/playlist_crawler');
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 const app = express();
 
